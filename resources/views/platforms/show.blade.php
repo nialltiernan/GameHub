4181d1b362ca6bfd768e3d7b20a73f0552ae61b9
Niall Tiernan <nialltiernan93@gmail.com>
2020-11-11 17:35:06 +0100

    Padding

--- resources/views/platforms/show.blade.php
+++ resources/views/platforms/show.blade.php
@@ -18,7 +18,7 @@
 
         <div class="flex-1">
             <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Highest critically rated games of {{ $selectedPlatform }}</h2>
-            <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+            <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16 pr-8">
                 @forelse($games as $game)
                     <div class="game mt-8">
                         <div class="relative inline-block">
