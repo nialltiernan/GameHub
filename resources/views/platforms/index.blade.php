385bd16e0f87981902237d860a62a367356be3a4
Niall Tiernan <nialltiernan93@gmail.com>
2020-11-12 16:31:57 +0100

    Update platform index layout

--- resources/views/platforms/index.blade.php
+++ resources/views/platforms/index.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <ul>
-            @foreach($platforms as $id => $platform)
-                <li><a href="{{ route('platforms.show', ['id' => $id]) }}">{{ $platform }}</a></li>
-            @endforeach
-        </ul>
-    </div> {{--end container--}}
+    <div class="container mx-auto px-8 flex flex-wrap">
+        @foreach($platforms as $id => $platform)
+            <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500 mr-4 pl-4 py-4 text-lg rounded">
+                <a
+                    class="hover:text-blue-600 transform"
+                    href="{{ route('platforms.show', ['id' => $id]) }}">
+                    {{ $platform }}
+                </a>
+            </div>
+        @endforeach
+    </div>
 @endsection
