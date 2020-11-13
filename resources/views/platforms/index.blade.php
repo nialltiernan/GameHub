11bb59b52f28d45f7441d2ee7e421698595b8569
Niall Tiernan <nialltiernan93@gmail.com>
2020-11-13 16:23:34 +0100

    Update platform index

--- resources/views/platforms/index.blade.php
+++ resources/views/platforms/index.blade.php
@@ -3,13 +3,15 @@
 @section('content')
     <div class="container mx-auto px-8 flex flex-wrap">
         @foreach($platforms as $id => $platform)
-            <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500 mr-4 pl-4 py-4 text-lg rounded">
                 <a
-                    class="hover:text-blue-600 transform"
+                    class="
+                        bg-gray-500 mr-4 pl-4 py-4 text-lg rounded
+                        transform hover:text-blue-600
+                        w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4"
+
                     href="{{ route('platforms.show', ['id' => $id]) }}">
                     {{ $platform }}
                 </a>
-            </div>
         @endforeach
     </div>
 @endsection
