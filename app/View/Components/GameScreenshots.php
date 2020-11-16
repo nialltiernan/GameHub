1992619fca37ea3caa77178da82434d4bfe011e7
Niall Tiernan <n.tiernan@alflex.nl>
2020-11-16 13:55:50 +0100

    Move screenshots to component

---
+++ app/View/Components/GameScreenshots.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class GameScreenshots extends Component
+{
+
+    /**
+     * @var array
+     */
+    public $game;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($game)
+    {
+        $this->game = $game;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('components.game-screenshots');
+    }
+}
--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -67,20 +67,7 @@
             </div>
         </div> {{--end game details--}}
 
-        <div class="images-container border-b border-gray-800 pb-12 mt-8">
-            <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Screenshots</h2>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-8">
-                @forelse($game['screenshots'] as $screenshot)
-                    <div>
-                        <a href="{{ $screenshot['url'] }}">
-                            <img src="{{ $screenshot['url'] }}" class="hover:opacity-75 transition ease-in-out duration-150" alt="screenshot">
-                        </a>
-                    </div>
-                @empty
-                    No screenshots available <p style="font-size:50px">&#129335;</p>
-                @endforelse
-            </div>
-        </div> {{--end images container--}}
+        <x-game-screenshots :game="$game"></x-game-screenshots>
 
         <div class="similar-games-container mt-8">
             <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Similar Games</h2>
