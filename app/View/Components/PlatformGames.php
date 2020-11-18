a4423c382655c934dca3a07d5d9387bddbc869c2
Niall Tiernan <nialltiernan93@gmail.com>
2020-11-18 18:13:19 +0100

    Add platform games component

---
+++ app/View/Components/PlatformGames.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class PlatformGames extends Component
+{
+
+    public array $game;
+
+    public function __construct(array $game)
+    {
+        $this->game = $game;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('components.platform-games');
+    }
+}
