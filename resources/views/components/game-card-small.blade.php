0dc7a1504cce235ea32c9698ade5f03d3d30f06d
Niall Tiernan <nialltiernan93@gmail.com>
2020-12-04 23:07:41 +0100

    Add max heights

--- resources/views/components/game-card-small.blade.php
+++ resources/views/components/game-card-small.blade.php
@@ -1,7 +1,7 @@
 <div class="game mt-4">
     <div class="relative inline-block">
         <a href="{{ route('game.show', ['id' => $game['id']]) }}">
-            <img src="{{ $game['image_url'] }}" class="hover:opacity-75" alt="similar game">
+            <img src="{{ $game['image_url'] }}" class="hover:opacity-75" alt="similar game" style="max-height: 200px">
         </a>
     </div>
 
--- resources/views/components/game-card.blade.php
+++ resources/views/components/game-card.blade.php
@@ -2,7 +2,7 @@
 
     <div class="relative inline-block">
         <a href="{{ route('game.show',['id' => $game['id']]) }}">
-            <img src="{{ $game['image_url'] }}" alt="game cover" class="hover:opacity-75">
+            <img src="{{ $game['image_url'] }}" alt="game cover" class="hover:opacity-75" style="max-height: 215px">
         </a>
 
         @if (isset($game['slug']))
