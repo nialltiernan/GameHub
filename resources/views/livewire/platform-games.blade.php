39523ea482d3dcf774214211555ceee5970726a5
Niall Tiernan <nialltiernan93@gmail.com>
2020-12-06 12:04:32 +0100

    Change margin top

--- resources/views/livewire/platform-games.blade.php
+++ resources/views/livewire/platform-games.blade.php
@@ -1,9 +1,11 @@
-<div wire:init="loadGames" class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 border-b border-gray-800 pb-16 pr-8">
-    @forelse($games as $game)
-        <x-platform-games :game="$game" />
-    @empty
-        @foreach(range(1,12) as $game)
-            <x-game-card-loading />
-        @endforeach
-    @endforelse
+<div wire:init="loadGames">
+    <div class="mt-8 popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-12 gap-y-6 border-b border-gray-800 pb-16 pr-8">
+        @forelse($games as $game)
+            <x-platform-games :game="$game" />
+        @empty
+            @foreach(range(1,12) as $game)
+                <x-game-card-loading />
+            @endforeach
+        @endforelse
+    </div>
 </div>
