d509e615c3853e07bc4d7b1897f7d5e5aea0c4a9
Niall Tiernan <nialltiernan93@gmail.com>
2020-12-20 16:03:38 +0100

    Check if user is logged in

--- resources/views/components/add-to-list-modal.blade.php
+++ resources/views/components/add-to-list-modal.blade.php
@@ -2,20 +2,22 @@
 
     @include('javascript.addToListModal')
 
-    <div class="w-1/2 m-auto">
-        <h2 class="font-semibold text-4xl leading-tight mt-1">Pick a list</h2>
-        @foreach(Auth::user()->lists as $list)
-            <form action="{{ route('listGame.store', ['user' => Auth::user(), 'list' => $list]) }}" method="post">
-                @csrf
-                <input type="hidden" name="gameId" value="{{ $gameId }}">
-                <input type="hidden" name="gameListId" value="{{ $list->id }}">
-                <input
-                        type="submit"
-                        class="bg-blue-500 text-white font-semibold px-2 py-2 hover:bg-blue-600 rounded"
-                        value="{{ $list->name }}">
-            </form>
-        @endforeach
+    @if (Auth::check())
+        <div class="w-1/2 m-auto">
+            <h2 class="font-semibold text-4xl leading-tight mt-1">Pick a list</h2>
+            @foreach(Auth::user()->lists as $list)
+                <form action="{{ route('listGame.store', ['user' => Auth::user(), 'list' => $list]) }}" method="post">
+                    @csrf
+                    <input type="hidden" name="gameId" value="{{ $gameId }}">
+                    <input type="hidden" name="gameListId" value="{{ $list->id }}">
+                    <input
+                            type="submit"
+                            class="bg-blue-500 text-white font-semibold px-2 py-2 hover:bg-blue-600 rounded"
+                            value="{{ $list->name }}">
+                </form>
+            @endforeach
 
-        <span class="text-6xl hover:text-gray-500 absolute" onclick="hideAddToListModal()">&times;</span>
-    </div>
+            <span class="text-6xl hover:text-gray-500 absolute" onclick="hideAddToListModal()">&times;</span>
+        </div>
+    @endif
 </div>
