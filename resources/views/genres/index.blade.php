3c7863dff1f41543d94c4907b6c50ddbbf4b22c5
Niall Tiernan <nialltiernan93@gmail.com>
2020-12-20 16:09:21 +0100

    Indentation

--- resources/views/genres/index.blade.php
+++ resources/views/genres/index.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <div class="container mx-auto px-8 flex flex-wrap">
         @foreach($genres as $id => $genre)
-                <a class="
-                        bg-gray-500 mr-4 pl-4 py-4 text-lg rounded
-                        transform hover:text-blue-600
-                        w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4"
-                    href="{{ route('genres.show', ['id' => $id]) }}"
-                >
-                    {{ $genre }}
-                </a>
+            <a class="
+                    bg-gray-500 mr-4 pl-4 py-4 text-lg rounded
+                    transform hover:text-blue-600
+                    w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4"
+                href="{{ route('genres.show', ['id' => $id]) }}"
+            >
+                {{ $genre }}
+            </a>
         @endforeach
     </div>
 @endsection
--- resources/views/platforms/index.blade.php
+++ resources/views/platforms/index.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <div class="container mx-auto px-8 flex flex-wrap">
         @foreach($platforms as $id => $platform)
-                <a class="
-                        bg-gray-500 mr-4 pl-4 py-4 text-lg rounded
-                        transform hover:text-blue-600
-                        w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4"
-                    href="{{ route('platforms.show', ['id' => $id]) }}"
-                >
-                    {{ $platform }}
-                </a>
-        @endforeach
+            <a class="
+                    bg-gray-500 mr-4 pl-4 py-4 text-lg rounded
+                    transform hover:text-blue-600
+                    w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4"
+                href="{{ route('platforms.show', ['id' => $id]) }}"
+            >
+                {{ $platform }}
+            </a>
+    @endforeach
     </div>
 @endsection
