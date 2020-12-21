db37ad7cd28820d1db34ef1750fa3de609a19f96
Niall Tiernan <nialltiernan93@gmail.com>
2020-12-21 11:59:29 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo '#### Moving to project directory ####'
+cd /home/niall/Projects/GameHub
+
+echo '#### Pulling code from git ####'
+git pull
+
+echo '#### Installing composer packages ####'
+composer install --optimize-autoloader --no-dev
+
+echo '#### Running migrations ####'
+php artisan migrate --force
+
+echo '#### Optimizing config cache ####'
+php artisan config:cache
+
+echo '#### Optimizing route cache ####'
+php artisan route:cache
+
+
+echo '#### Optimizing view cache ####'
+php artisan view:cache
+
+echo '#### Deployment successful ####'
