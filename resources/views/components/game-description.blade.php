1638c873bc5bc77ec1cda6684ddd97c0c7c83ccd
Niall Tiernan <nialltiernan93@gmail.com>
2020-12-21 17:55:01 +0100

    Move social links beside title on large displays

--- resources/views/components/game-description.blade.php
+++ resources/views/components/game-description.blade.php
@@ -5,7 +5,7 @@
 
     <div>
         <div id="descriptionPreview">
-            <p class="mt-12">{{ $preview }}
+            <p class="mt-6">{{ $preview }}
                 <span
                     class="bg-blue-500 text-white font-semibold px-1 hover:bg-blue-600 rounded"
                     onclick="descriptionShowFull()">Show more
@@ -14,7 +14,7 @@
         </div>
 
         <div id="descriptionFull" class="hidden">
-            <p class="mt-12">{{ $full }}
+            <p class="mt-6">{{ $full }}
                 <span
                     class="bg-blue-500 text-white font-semibold px-1 hover:bg-blue-600 rounded"
                     onclick="descriptionShowPreview()">Show less
--- resources/views/components/game-details.blade.php
+++ resources/views/components/game-details.blade.php
@@ -1,75 +1,82 @@
-<div class="game-details border-b border-gray-800 pb-12 flex flex-col lg:flex-row">
+<div class="game-details border-b border-gray-800 pb-12">
     <x-add-to-list-modal :game-id="$game['id']"/>
 
-    <div>
-        <h2 class="font-semibold text-4xl leading-tight mt-1">
-            @if(isset($game['website']) && $game['website'])
-                <a href="{{ $game['website'] }}" target="_blank" class="hover:text-gray-400">{{ $game['name'] }}</a>
-            @else
-                {{ $game['name'] }}
-            @endif
-        </h2>
-        <div class="text-gray-400">
+    <div class="flex lg:flex-row justify-between flex-wrap">
+        <div class="mb-2">
+            <h2 class="font-semibold text-4xl leading-tight mt-1">
+                @if(isset($game['website']) && $game['website'])
+                    <a href="{{ $game['website'] }}" target="_blank" class="hover:text-gray-400">{{ $game['name'] }}</a>
+                @else
+                    {{ $game['name'] }}
+                @endif
+            </h2>
+            <div class="text-gray-400">
 
             <span>
                 @foreach($game['genres'] as $genreId => $genre)
-                     <a href="{{ route('genres.show', ['id' => $genreId]) }}" class="hover:text-blue-600">
+                    <a href="{{ route('genres.show', ['id' => $genreId]) }}" class="hover:text-blue-600">
                         {{ $genre }}@if (!$loop->last),@endif
                      </a>
                 @endforeach
             </span>
 
-            @if($game['publisher'])
+                @if($game['publisher'])
                 &middot; <span>{{ $game['publisher'] }}</span> &middot;
-            @endisset
+                @endisset
 
-            @if($game['released'])
-                <span>{{ $game['released'] }}</span> &middot;
-            @endisset
+                @if($game['released'])
+                    <span>{{ $game['released'] }}</span> &middot;
+                @endisset
 
-            <span>
+                <span>
                 @foreach($game['platforms'] as $platformId => $platform)
-                    <a href="{{ route('platforms.show', ['id' => $platformId]) }}" class="hover:text-blue-600">
+                        <a href="{{ route('platforms.show', ['id' => $platformId]) }}" class="hover:text-blue-600">
                         {{ $platform }}@if (!$loop->last),@endif
                     </a>
-                @endforeach
+                    @endforeach
             </span>
-        </div>
-        <div class="flex flex-wrap items-center mt-8">
-            <div class="flex items-center">
-                @if ($game['rating'])
-                    <div id="ratingDiv" class="w-16 h-16 bg-gray-800 rounded-full relative text-sm mr-4">
-                        @push('scripts')
-                            @include('javascript.rating', [
-                                'slug' => 'ratingDiv',
-                                'rating' => $game['rating'],
-                                'event' => null
-                            ])
-                        @endpush
-                    </div>
-                @endif
             </div>
+        </div>
 
-            <div class="flex items-center space-x-4">
-                @foreach($game['social_links'] as $category => $link)
-                    <a href="{{ $link }}" target="_blank">
-                        <img class="h-10 transform hover:scale-150" src="/images/icons/{{ $category }}.svg" alt="{{ $category }}">
-                    </a>
-                @endforeach
+        <div>
+            <div class="flex items-center mt-8 lg:mt-0 lg:flex-row-reverse">
+
+                <div class="flex items-center">
+                    @if ($game['rating'])
+                        <div id="ratingDiv" class="w-16 h-16 bg-gray-800 rounded-full relative text-sm mr-4 lg:ml-4">
+                            @push('scripts')
+                                @include('javascript.rating', [
+                                    'slug' => 'ratingDiv',
+                                    'rating' => $game['rating'],
+                                    'event' => null
+                                ])
+                            @endpush
+                        </div>
+                    @endif
+                </div>
+
+                <div class="flex items-center space-x-4">
+                    <livewire:franchise-link :game="$game['name']"/>
+
                     <form onsubmit="return false">
                         <input
                             title="Add to list"
                             type="image"
                             src="/images/icons/plus.svg"
                             onclick="showAddToListModal()"
-                            class="h-8 transform hover:scale-150"
-                        >
+                            class="h-8 transform hover:scale-150">
                     </form>
-                    <livewire:franchise-link :game="$game['name']"/>
+
+                    @foreach($game['social_links'] as $category => $link)
+                        <a href="{{ $link }}" target="_blank">
+                            <img class="h-10 transform hover:scale-150" src="/images/icons/{{ $category }}.svg" alt="{{ $category }}">
+                        </a>
+                    @endforeach
+                </div>
             </div>
-
-            <x-game-description :preview="$game['description']['preview']" :full="$game['description']['full']" />
-
         </div>
     </div>
+
+    <x-game-description :preview="$game['description']['preview']" :full="$game['description']['full']" />
+
 </div>
