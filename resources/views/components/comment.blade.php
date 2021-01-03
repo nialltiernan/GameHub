3e98aae8f3cc9f8cda7626905d1646c5c268240f
Niall Tiernan <nialltiernan93@gmail.com>
2021-01-03 15:06:16 +0100

    Frontend change

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,4 +1,6 @@
 <div class="flex justify-between bg-gray-600 p-4 mb-2 rounded">
     <span>{{ $comment->message }}</span>
-    <span class="text-sm">Posted by {{ $comment->user->username }} on {{ $comment->created_at->format('M d Y') }}</span>
+    <span class="text-sm">
+        <span class="italic">{{ $comment->user->username }}</span> at {{ $comment->created_at->format('M d Y') }}
+    </span>
 </div>
