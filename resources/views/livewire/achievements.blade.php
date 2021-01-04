6808a443a8d8c91b2d5161325009863fcb73a11a
Niall Tiernan <nialltiernan93@gmail.com>
2021-01-04 19:11:06 +0100

    Support mobile

--- resources/views/livewire/achievements.blade.php
+++ resources/views/livewire/achievements.blade.php
@@ -1,6 +1,6 @@
 <div wire:init="loadAchievements" class="border-b border-gray-800 pb-12 mt-8">
     <h1>Achievements</h1>
-    <div class="grid grid-cols-3 gap-4 mt-8">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
         @forelse($achievements as $achievement)
             <x-achievement :achievement="$achievement" />
         @empty
