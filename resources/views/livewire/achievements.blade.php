68c48c01d4bc5fd9121e1cf8ea9cc3b7d90d3fd1
Niall Tiernan <nialltiernan93@gmail.com>
2021-01-11 07:29:56 +0100

    Frontend styling achievements

--- resources/views/livewire/achievements.blade.php
+++ resources/views/livewire/achievements.blade.php
@@ -1,12 +1,9 @@
 <div wire:init="loadAchievements" class="border-b border-gray-800 pb-12 mt-8">
-    <div class="">
-        <h1>
-            Achievements (
-                <a href="{{ route('game.achievements', ['id' => $gameId]) }}" class="hover:text-gray-400 underline">
-                    See all
-                </a>
-            )
-        </h1>
+    <div class="flex justify-between flex-col sm:flex-row">
+        <h1>Achievements</h1>
+        <a href="{{ route('game.achievements', ['id' => $gameId]) }}" class="hover:text-gray-400 underline">
+            See all achievements
+        </a>
     </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
         @forelse($achievements as $achievement)
