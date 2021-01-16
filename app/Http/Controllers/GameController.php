928199535f15cc7e17d7d420584d6c9d89a8bf90
Niall Tiernan <nialltiernan93@gmail.com>
2021-01-16 11:56:03 +0100

    Validate game route

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Services\Rawg\ClientRetriever;
 use App\View\Models\GameViewModel;
+use Validator;
 
 class GameController extends Controller
 {
     public function show($id, ClientRetriever $clientRetriever)
     {
+        $validator = Validator::make(['id' => $id], ['id' => 'numeric',]);
+
+        if ($validator->fails()) {
+            abort(404);
+        }
+
         $client = $clientRetriever->executeNoAuth();
 
         $data = $client->games()->getGame($id)->getData();
